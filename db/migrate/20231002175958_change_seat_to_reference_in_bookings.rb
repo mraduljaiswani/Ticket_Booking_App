b92f20a9c5aeb85ff21be528843e21e20ccff809
mraduljaiswani <mradul.jaiswani@gmail.com>
2023-10-07 13:25:08 +0530

    removing column for seats and venue_id

--- db/migrate/20231002175958_change_seat_to_reference_in_bookings.rb
+++ db/migrate/20231002175958_change_seat_to_reference_in_bookings.rb
@@ -1,6 +1,5 @@
 class ChangeSeatToReferenceInBookings < ActiveRecord::Migration[7.0]
   def change
-    remove_column :bookings, :selected_seats, :text
     add_reference :bookings, :seat, foreign_key: true
 
     
--- db/migrate/20231002181303_change_venue_to_reference_in_seats.rb
+++ db/migrate/20231002181303_change_venue_to_reference_in_seats.rb
@@ -1,6 +1,5 @@
 class ChangeVenueToReferenceInSeats < ActiveRecord::Migration[7.0]
   def change
-    remove_column :seats, :venue_id, :integer
     add_reference :seats, :venue, foreign_key: true
   
   end
